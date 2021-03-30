d10cdb8cbf7e908a2a3163c89b01a8143f1b9144
Laura Fernandes <lauqfernandes@gmail.com>
2021-03-30 12:14:35 -0300

    changes

--- func.js
+++ func.js
@@ -12,30 +12,34 @@ function resumo() {
 
     if(nome === "" || telefone === "" || email === "")
     {
-        alert("Os dados pessoais devem ser digitados!!!");
+        document.getElementById("resumo").innerHTML = 
+        "Os dados pessoais devem ser digitados!!!";
     }
     else{
 
         document.getElementById("resumo").innerHTML = 
-        `<div class="alert alert-secondary resumo">
-        <h2> Reveja seus dados e pedido: </h2>
+        `<div resumo">
+        <h2> Reveja seus dados e pedido: </h2><br>
          
         <h3>Dados pessoais: </h3><br>
-        Cliente: ${nome}, Telefone: ${telefone}, Email: ${email} <br><br>
+        Cliente: ${nome}<br>
+        Telefone: ${telefone}<br> 
+        Email: ${email} <br><br>
         
         <h3>Pedido: </h3><br>
         <h5>Prato principal:</h5>${pratoPrincipal('prato_principal')}<br><br>
-        <h5>Acompanhamentos:</h5> ${acompanhamentos('option_a')}
+        <h5>Acompanhamentos:</h5> ${acompanhamentos('option_a')}<br><br>
+
+        <h3>Convênio</h3>
+        Convenio: ${Desconto("option_b")}
     
         <br><br><h2>Total: </h2>
-        <h3>R$${total},00</h3>
-    
+        <h3>R$${total}</h3>
+
         
-        <button onclick="Limpar()" type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+        <button type="button" data-bs-dismiss="modal" onclick="Limpar()"  class="btn btn-success" data-bs-toggle="modal" data-bs-target="#mymodal2">
         Confirmar
         </button>
-    
-        <button onclick="Limpar()" class="btn btn-danger">Limpar tudo</button>
     
         </div>`
 
@@ -60,14 +64,14 @@ function  pratoPrincipal(name){
 
     total = total + pratoEscolhido[escolhido].preço;
 
-    return `<br>${pratoEscolhido[escolhido].nome}..................R$${pratoEscolhido[escolhido].preço},00`;
+    return `<br>${pratoEscolhido[escolhido].nome}..................R$${pratoEscolhido[escolhido].preço}`;
 }
 
 
 function acompanhamentos(name){
     var checkboxes = document.getElementsByName(name);
     var escolhidos = [];
-    var va = 8, vs = 8, vb=10, vm = 12, count = 0; 
+    var va = 8.00, vs = 8.00, vb=10.00, vm = 12.00, count = 0; 
 
     let acompanhamentosEscolhidos = [
         {nome: "Arroz", preço:va},
@@ -79,7 +83,7 @@ function acompanhamentos(name){
     for(var i = 0; i < checkboxes.length; i++){
         if(checkboxes[i].checked){
             count++;
-            escolhidos.push(`<br>${acompanhamentosEscolhidos[i].nome}........................R$${acompanhamentosEscolhidos[i].preço},00`);
+            escolhidos.push(`<br>${acompanhamentosEscolhidos[i].nome}........................R$${acompanhamentosEscolhidos[i].preço}`);
             total = total + acompanhamentosEscolhidos[i].preço;
         }
     }
@@ -89,11 +93,36 @@ function acompanhamentos(name){
     }
 
     return escolhidos;
-        
 }
 
 function Limpar(){
-    total = 0;
+    total = 0.00;
     document.querySelector('form').reset();
     document.getElementById("resumo").innerHTML = ""
 }
+
+function Desconto(name){
+    var select = document.getElementsByName(name);
+    var escolhido;
+    let conveniosCadastrados = [
+        {nome: "Nenhum", desconto: 0},
+        {nome: "Empresa X", desconto: 0.10},
+        {nome: "Empresa Y", desconto: 0.15},
+    ]
+
+    for(var i = 0; i < select.length; i++){
+        if(select[i].selected){
+            escolhido = select[i].value;
+        }
+    }
+
+    totalDesconto = total*conveniosCadastrados[escolhido].desconto
+
+    total = total - (totalDesconto);
+
+    return `${conveniosCadastrados[escolhido].nome}, Desconto de: R$${totalDesconto}`;
+
+}
+
+
+
